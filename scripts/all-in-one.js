67c870a37cb5231c9d4ad416100896ff3ae30db9
FAHIM MURSHED PRANTO <fahimmurshedpranto@gmail.com>
2024-02-20 14:37:42 +0600

    fornt end part done with html and css

---
+++ scripts/all-in-one.js
@@ -0,0 +1,5 @@
+// function hideHomeScreenById(homeId)
+// {
+//    const home = document.getElementById('home-screen');
+//    home.classList.add('hidden');
+// }
---
+++ scripts/function-call.js
@@ -0,0 +1,4 @@
+function buyTicket(){
+    // hideHomeScreenById('home-screen');
+    
+}
